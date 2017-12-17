d8581a564408b4d0685e147054f60aee368ec1c3
gejimayu <gianffh@gmail.com>
2017-12-17 14:22:27 +0700

    Add coupon model

---
+++ api/models/coupon.js
@@ -0,0 +1,36 @@
+const mongoose 	= require('mongoose');
+
+const couponSchema = new mongoose.Schema({
+	desc: {
+		type: String
+	},
+
+	code: {
+		type: String,
+		required: true,
+		uppercase: true
+	},
+
+	percentage_discount: {
+		type: Number,
+		required: true,
+		min: 0,
+		max: 100
+	},
+
+	quantity: {
+		type: Number,
+		required: true
+	},
+
+	start_valid: {
+		type: Date,
+		required: true
+	},
+
+	end_valid: {
+		type: Date,
+		required: true
+	}
+});
+module.exports = mongoose.model('Product', productSchema);
